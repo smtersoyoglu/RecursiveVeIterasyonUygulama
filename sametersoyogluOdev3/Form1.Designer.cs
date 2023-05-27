62ef9942eb136e1f87d324a0d5b6883dda4aca40
smter <smter@DESKTOP-96JC8IR>
2023-05-27 17:17:07 +0300

    Add project files.

---
+++ sametersoyogluOdev3/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace sametersoyogluOdev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_Sayi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtBox_recursive = new System.Windows.Forms.TextBox();
+            this.txtBox_iterasyon = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox_sayilar = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtBox_Sayi
+            // 
+            this.txtBox_Sayi.Location = new System.Drawing.Point(40, 103);
+            this.txtBox_Sayi.Multiline = true;
+            this.txtBox_Sayi.Name = "txtBox_Sayi";
+            this.txtBox_Sayi.Size = new System.Drawing.Size(166, 26);
+            this.txtBox_Sayi.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(37, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sayıları Gir";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEkle.Location = new System.Drawing.Point(40, 139);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(80, 42);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(126, 139);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(80, 42);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(343, 56);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(88, 44);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtBox_recursive
+            // 
+            this.txtBox_recursive.Location = new System.Drawing.Point(221, 72);
+            this.txtBox_recursive.Multiline = true;
+            this.txtBox_recursive.Name = "txtBox_recursive";
+            this.txtBox_recursive.Size = new System.Drawing.Size(100, 25);
+            this.txtBox_recursive.TabIndex = 6;
+            // 
+            // txtBox_iterasyon
+            // 
+            this.txtBox_iterasyon.Location = new System.Drawing.Point(451, 72);
+            this.txtBox_iterasyon.Multiline = true;
+            this.txtBox_iterasyon.Name = "txtBox_iterasyon";
+            this.txtBox_iterasyon.Size = new System.Drawing.Size(100, 25);
+            this.txtBox_iterasyon.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(218, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Recursive Sonuç";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(448, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "İterasyon Sonuç";
+            // 
+            // richTextBox_sayilar
+            // 
+            this.richTextBox_sayilar.Location = new System.Drawing.Point(221, 106);
+            this.richTextBox_sayilar.Name = "richTextBox_sayilar";
+            this.richTextBox_sayilar.Size = new System.Drawing.Size(330, 78);
+            this.richTextBox_sayilar.TabIndex = 10;
+            this.richTextBox_sayilar.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(617, 244);
+            this.Controls.Add(this.richTextBox_sayilar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBox_iterasyon);
+            this.Controls.Add(this.txtBox_recursive);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox_Sayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_Sayi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtBox_recursive;
+        private System.Windows.Forms.TextBox txtBox_iterasyon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox_sayilar;
+    }
+}
+
---
+++ sametersoyogluOdev3/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace sametersoyogluOdev3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // standart sapma bulma adımları n tane girilen sayının ilk ortalamasını bulma, sonra varyansı bulma ortalama sonucunu girilen sayılardan tek tek cıkarma
+        // cıkardıktan sonra her değerden gelen sonuçların karesini alma  karesi alındıktan sonra karesi alınan sayıları toplama = ortFarkKareTop, son olarak
+        //Kareleri toplamını n-1'e bölme cıkan varyans değerinin karekök'ünü alma
+
+        ArrayList sayiDizi = new ArrayList();
+
+        private double girilenSayi;
+        private double girSayiTop;
+        private double ortalama;
+        private double ortFarkKareTop;
+        private double standartSapma;
+        private double varyans;
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (txtBox_Sayi.Text == "")
+            {
+                MessageBox.Show("Boş Bırakılamaz Sayı Girmeniz Gereklidir !");
+            }
+            else
+            {
+                sayiDizi.Add(int.Parse(txtBox_Sayi.Text));
+                richTextBox_sayilar.Text += txtBox_Sayi.Text + " <---> ";
+                txtBox_Sayi.Clear();
+            }
+        }
+
+        public double recursiveOrtalamaBulma(int n)
+        {
+            if (n == 0)
+            { return 0; }
+
+            // n-1 sayı dizisinde ortalamayı bulma 
+            double girilenSayi = double.Parse(sayiDizi[n - 1].ToString());
+            return (recursiveOrtalamaBulma(n - 1) * (n - 1) + girilenSayi) / n;
+        }
+
+        public double recursiveToplamBulma(int n, double ortalama)
+        {
+            if (n == 0)
+            { return 0; }
+
+            double girilenSayi = double.Parse(sayiDizi[n - 1].ToString());
+            //Math.Pow ile üs alma işlemini yapıyoruz (girilenSayi - ortalama) taban üst 2 oluyor karesini alma işlemi
+            return Math.Pow(girilenSayi - ortalama, 2) + recursiveToplamBulma(n - 1, ortalama);
+
+        }
+
+        public double recursiveHesaplama()
+        {
+
+            int n = sayiDizi.Count;
+            ortalama = recursiveOrtalamaBulma(n);
+            ortFarkKareTop = recursiveToplamBulma(n, ortalama);
+            varyans = 1 / (Convert.ToDouble(n) - 1) * ortFarkKareTop;
+            //Math.Pow ile üs alma işlemini yapıyoruz (girilenSayi - ortalama) taban üst 2 oluyor karesini alma işlemi
+            ortFarkKareTop += Math.Pow(girilenSayi - ortalama, 2);
+            //Math.Sqrt ile son işlem olarak karakökünü alıyoruz. karakök hesaplama
+            standartSapma = Math.Sqrt(varyans);
+            standartSapma = Math.Round(standartSapma, 2);
+            return standartSapma;
+
+        }
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            // iterasyon hesaplama
+            int n = sayiDizi.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                girilenSayi = double.Parse(sayiDizi[i].ToString());
+                girSayiTop = girSayiTop + girilenSayi;
+            }
+
+            // ortalama bulma
+            ortalama = girSayiTop / n;
+            for (int i = 0; i < n; i++)
+            {
+                double girilenSayi = double.Parse(sayiDizi[i].ToString());
+                //Math.Pow ile üs alma işlemini yapıyoruz (girilenSayi - ortalama) taban üst 2 oluyor karesini alma işlemi
+                ortFarkKareTop = ortFarkKareTop + Math.Pow(girilenSayi - ortalama, 2);
+            }
+            //Math.Sqrt ile son işlem olarak karakökünü alıyoruz. karakök hesaplama
+            standartSapma = Math.Sqrt(ortFarkKareTop / (n - 1));
+            standartSapma = Math.Round(standartSapma, 2);
+            txtBox_iterasyon.Text = standartSapma.ToString();
+
+            // recursive hesaplama 
+            txtBox_recursive.Text = recursiveHesaplama().ToString();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+
+            txtBox_Sayi.Clear();
+            txtBox_recursive.Clear();
+            txtBox_iterasyon.Clear();
+            richTextBox_sayilar.Clear();
+
+        }
+
+    }
+}
